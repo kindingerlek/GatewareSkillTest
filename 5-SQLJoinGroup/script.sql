ed7546b20927f0437c79b6669cd0e918347ec1ac
Lucas Ernesto Kindinger <ernestokindinger@bne.com.br>
2019-05-26 23:19:02 -0300

    fix sql scripts

--- 5-SQLJoinGroup/script.sql
+++ 5-SQLJoinGroup/script.sql
@@ -1,15 +1,16 @@
 
-
 -- METHOD 1
-SELECT Nome_Colaborador,sum(Valor_Pago) 
-FROM Colaboradores c 
-JOIN Salarios s ON s.ID_SALARIO = c.ID_SALARIO
-WHERE DATA_PAGAMENTO BETWEEN '2014-01-01' AND '2014-12-31'
-GROUP BY Nome_Colaborador
+SELECT c.nome_colaborador,sum(s.valor_pago) as soma_salario, lc.uf as estados
+FROM salarios S
+INNER JOIN colaboradores C on C.id_colaborador = S.id_colaborador
+INNER JOIN local_de_trabalho lc ON lc.id_colaborador = s.id_colaborador
+WHERE s.data_pagamento BETWEEN '2014-01-01 00:00' AND '2014-12-31 23:59'
+GROUP BY c.nome_colaborador, lc.uf
 
 -- METHOD 2
-SELECT Nome_Colaborador,SUM(Valor_Pago) 
-FROM Colaboradores c 
-JOIN Salarios s ON s.ID_SALARIO = c.ID_SALARIO
-WHERE YEAR(DATA_PAGAMENTO) = 2014
-GROUP BY Nome_Colaborador
+SELECT c.nome_colaborador,sum(s.valor_pago) as soma_salario, lc.uf as estados
+FROM salarios S
+INNER JOIN colaboradores C on C.id_colaborador = S.id_colaborador
+INNER JOIN local_de_trabalho lc ON lc.id_colaborador = s.id_colaborador
+WHERE YEAR(s.data_pagamento) = 2014
+GROUP BY c.nome_colaborador, lc.uf
